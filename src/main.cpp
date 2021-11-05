b08c0b4e8a8cc056141428d949a79f0dadba6fdc
Wojciech Majewski <wojciech.majewski@pm.me>
2021-11-05 17:52:15 +0100

    WIP random color

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,27 @@
 #define DATA_PIN 6
 #define FADE_AMOUNT 10
 
-int minBrightness = 100;
-int brightness = minBrightness;
-int fadeAmount = 2;
+uint8_t minBrightness = 0;
+uint8_t maxBrightness = 255;
+uint8_t brightness = maxBrightness;
+uint8_t fadeAmount = 1;
 
 CRGB leds[NUM_LEDS];
+CRGB selectedColor;
+
+// nice colors: CRGB::LawnGreen, CRGB::Teal
+CRGB colorRedstone = CRGB(255, 0, 0);
+CRGB colorEmerald = CRGB(0, 255, 0);
+CRGB colorLapisLazuli = CRGB(0, 0, 255);
+CRGB colorGold = CRGB(255, 100, 0);
+CRGB colorDiamond = CRGB(100, 245, 228);
+CRGB availableColors[5] = { colorRedstone, colorEmerald, colorLapisLazuli, colorGold, colorDiamond };
 
 void setup() {
   Serial.begin(9600);
+  // selectedColor = colorRedstone;
+  // selectedColor = CHSV(random8(), 255, 255);
+  selectedColor = availableColors[random(5)];
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
@@ -21,21 +34,27 @@ void setup() {
   FastLED.show();
 }
 
+
 void loop() {
   for (size_t i = 0; i < NUM_LEDS; i++)
   {
-    // leds[i] = CRGB::LawnGreen;
-    leds[i] = CRGB::Teal;
-    leds[i].fadeLightBy(brightness);
+    leds[i] = selectedColor;
+    leds[i].fadeLightBy(255 - brightness);
   }
   FastLED.show();
 
-  if (brightness + fadeAmount <= minBrightness || brightness + fadeAmount >= 255) {
+  if (brightness + fadeAmount <= minBrightness || brightness + fadeAmount >= maxBrightness) {
     fadeAmount = -fadeAmount;
   }
   brightness = brightness + fadeAmount;
 
-  delay(50);
+  delay(5);
 
-  Serial.println(brightness);
+  String stringAry[5] = {"A", "B", "C", "D", "E"};
+  long randomNumber = random(5);
+  String selectedString = stringAry[randomNumber];
+
+  Serial.println(selectedColor);
+  // Serial.print(randomNumber); Serial.print(" "); Serial.println(selectedString);
+  // Serial.print("long = "); Serial.print(randomNumber); Serial.print(", casted = "); Serial.println((uint8_t) randomNumber);
 }
