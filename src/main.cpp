c70d25acffbf7255550c5f0e169b9240f39b4e6a
Wojciech Majewski <wojciech.majewski@pm.me>
2021-11-10 11:59:05 +0100

    Fix fading properly

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 #define DATA_PIN 6
 #define FADE_AMOUNT 10
 
-uint8_t minBrightness = 0;
-uint8_t maxBrightness = 255;
-uint8_t brightness = maxBrightness;
-uint8_t fadeAmount = 1;
+uint8_t minFade = 0;
+uint8_t maxFade = 180;
+uint8_t currentFade = maxFade;
+int8_t fadeStep = 1;
 
 CRGB leds[NUM_LEDS];
 CRGB selectedColor;
@@ -24,8 +24,6 @@ CRGB availableColors[5] = { colorRedstone, colorEmerald, colorLapisLazuli, color
 
 void setup() {
   Serial.begin(9600);
-  // selectedColor = colorRedstone;
-  // selectedColor = CHSV(random8(), 255, 255);
   selectedColor = availableColors[random(5)];
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, GRB>(leds, NUM_LEDS);
@@ -39,22 +37,20 @@ void loop() {
   for (size_t i = 0; i < NUM_LEDS; i++)
   {
     leds[i] = selectedColor;
-    leds[i].fadeLightBy(255 - brightness);
+    leds[i].fadeLightBy(currentFade);
   }
   FastLED.show();
 
-  if (brightness + fadeAmount <= minBrightness || brightness + fadeAmount >= maxBrightness) {
-    fadeAmount = -fadeAmount;
+  if (currentFade + fadeStep <= minFade || currentFade + fadeStep >= maxFade) {
+    fadeStep = -fadeStep;
   }
-  brightness = brightness + fadeAmount;
+  currentFade = currentFade + fadeStep;
 
-  delay(5);
+  delay(15);
 
   String stringAry[5] = {"A", "B", "C", "D", "E"};
   long randomNumber = random(5);
   String selectedString = stringAry[randomNumber];
 
-  Serial.println(selectedColor);
-  // Serial.print(randomNumber); Serial.print(" "); Serial.println(selectedString);
-  // Serial.print("long = "); Serial.print(randomNumber); Serial.print(", casted = "); Serial.println((uint8_t) randomNumber);
+  Serial.println(currentFade);
 }
