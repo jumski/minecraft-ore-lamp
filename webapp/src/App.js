b901272c74c05903107849dce1370adb941192b6
Wojtek Majewski <jumski@gmail.com>
2021-11-10 15:33:51 +0100

    Working sending color via serial

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,10 +4,22 @@ import { useState } from 'react';
 import setupReader from './setupReader';
 import writeToPort from './writeToPort';
 
+const PREDEFINED_COLORS = [
+  { name: 'Redstone', color: { r: 255, g: 0, b: 0 } },
+  { name: 'Emerald', color: { r: 0, g: 255, b: 0 } },
+  { name: 'Lapis Lazuli', color: { r: 0, g: 0, b: 255 } },
+  { name: 'Gold', color: { r: 255, g: 100, b: 0 } },
+  { name: 'Diamond', color: { r: 100, g: 245, b: 228 } },
+]
+
 function App() {
   let [serialPort, setSerialPort] = useState(false);
-  let [color, setColor] = useState({ r: 255, g: 0, b: 0 });
+  // let [color, setColor] = useState({ r: 255, g: 0, b: 0 });
   let [readColor, setReadColor] = useState(false);
+
+  function writeColor({ r, g, b }) {
+    writeToPort(serialPort, [r, g, b].join(',') + "\n");
+  }
 
   async function connectButtonClicked() {
     let port = await navigator.serial.requestPort({});
@@ -25,16 +37,28 @@ function App() {
     });
   }
 
-  function onPickedColor(color) {
-    setColor(color.rgb);
-    console.log(color.rgb);
-  }
+//   function onPickedColor(color) {
+//     setColor(color.rgb);
+//     console.log(color.rgb);
+//   }
+
+  const predefinedColorButtons = PREDEFINED_COLORS.map(({ name, color }) => {
+    let bgColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    return <button
+      onClick={() => writeColor(color)}
+      key={name}
+      style={{backgroundColor: bgColor}}>
+      { name }
+    </button>;
+  });
 
   if (readColor) {
     return (
       <div className="App">
         <strong>Read Color:</strong><br/>
         <pre type="code">{JSON.stringify(readColor, null, 2)}</pre>
+
+        {predefinedColorButtons}
 
         {/* <SketchPicker color={color} onChange={onPickedColor}/> */}
       </div>
