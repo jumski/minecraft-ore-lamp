340cc54c8fc508bbd0fa0e5084fdb3406edb381e
Wojtek Majewski <jumski@gmail.com>
2021-11-10 15:09:01 +0100

    Import react-color and make it working

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { SketchPicker } from 'react-color';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState({ r: 255, g: 0, b: 0 });
+
+  function onPickedColor(color) {
+    setColor(color.rgb);
+    console.log(color.rgb);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SketchPicker color={color} onChange={onPickedColor}/>
     </div>
   );
 }
